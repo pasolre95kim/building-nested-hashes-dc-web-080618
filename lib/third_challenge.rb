ee7be84b0b22353991a0a88a2bb3a86573841747
Michelle Kim <pasolre95kim@gmail.com>
2018-07-15 17:27:11 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,8 +2,8 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name = "Lord Montague"
-        :age = "54"
+        :name: "Lord Montague"
+        :age: "54"
         },
       :matriarch => {
         #your key/value pairs here
