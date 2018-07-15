7c221b8306ca69b773c40380a8473921b230bb56
Michelle Kim <pasolre95kim@gmail.com>
2018-07-15 17:33:27 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,19 @@
 def second_challenge
   epic_tragedy = {
-   :montague => {
+    :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-   },
-   :capulet => {
+      #your key/value pairs here!
+      },
+    :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => []
-   }
-}
+      :heroine_friends =>[]
+      #your key/value pairs here!
+      }
+    }
+
 end
