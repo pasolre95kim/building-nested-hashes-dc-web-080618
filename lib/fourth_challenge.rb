695a32fc682574f1541e1b9a1a9ec26c10d96e5f
Michelle Kim <pasolre95kim@gmail.com>
2018-07-15 17:48:41 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"},
-      {name: "Steven", age: "30", attitude: "confused"}
+        {name: "Mercutio", age: "18", attitude: "hot-headed"},
+
         #your hashes here!
       ]
    },
@@ -13,8 +14,9 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{name: "Mercutio", age: "18", attitude: "hot-headed"},
+      :heroine_friends => [
         {name: "Nurse", age: "44", attitude: "worried"}
+        {name: "Steven", age: "30", attitude: "confused"}
         #your hashes here!
       ]
    }
